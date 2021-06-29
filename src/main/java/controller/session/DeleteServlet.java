3025528e9a95b753465d6e8722e2d9e484cd3e62
佐藤綜一郎 <sato.soichiro@unizonbex.com>
2021-06-29 16:44:43 +0900

    sessionについて微調整

--- src/main/java/controller/session/DeleteServlet.java
+++ src/main/java/controller/session/DeleteServlet.java
@@ -1,5 +1,6 @@
 package controller.session;
 
+import model.enter.Repository;
 import model.user.User;
 
 import javax.servlet.RequestDispatcher;
@@ -8,11 +9,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/sessions/delete")
 public class DeleteServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("currentUser");
+        Repository.deleteEnter(user);
+
         User.logoutUser(request);
         response.sendRedirect("/sessions/new");
     }
