6dbbad84deef1659674a487cec954a262a3852e6
佐藤綜一郎 <sato.soichiro@unizonbex.com>
2021-11-02 01:30:30 +0900

    フィルターの方法を変更

--- src/main/java/filter/AuthenticateFilter.java
+++ src/main/java/filter/AuthenticateFilter.java
@@ -112,18 +112,20 @@ public class AuthenticateFilter implements Filter {
                 httpServletResponse.sendRedirect("/users");
             }
         }
+        else if (servletPath.equals("/")) {
+            if (currentUser == null) {
+                httpServletResponse.sendRedirect("/index");
+            } else {
+                httpServletResponse.sendRedirect("/users");
+            }
+        }
         // ログインしないとダメな画面にアクセスしたとき
         else {
             if (currentUser == null) {
                 // リダイレクト
                 httpServletResponse.sendRedirect("/index");
-            } else {
-                if (servletPath.equals("/")) {
-                    httpServletResponse.sendRedirect("/users");
-                } else {
-                    chain.doFilter(req, resp);
-                }
             }
+                chain.doFilter(req, resp);
         }
     }
 
